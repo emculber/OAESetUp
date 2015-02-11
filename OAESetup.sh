1d3ea511b52fed73a3d6edd3fa34582f2f1d2f31
Erik Culberson <eculberson12@gmail.com>
2015-02-11 16:54:26 -0500

    More support for CentOS

--- OAESetup.sh
+++ OAESetup.sh
@@ -5,85 +5,121 @@ mkdir ~/OAE
 echo 'Change dir entering ~/Downloads'
 cd ~/Downloads
 
-#pre-processes
-su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm'
-
-#Command line installs
-sudo yum install erlang
-sudo yum group install "Development Tools"
-
-#Download all files
-wget http://nodejs.org/dist/v0.10.36/node-v0.10.36-linux-x64.tar.gz #Node.js
-wget http://download.nextag.com/apache/cassandra/2.1.2/apache-cassandra-2.1.2-bin.tar.gz #Apache Cassandra
-wget sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-"http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jre-8u25-linux-x64.tar.gz" #Orical Java 7
-wget http://download.redis.io/releases/redis-2.8.19.tar.gz #Redis
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.2.tar.gz #ElasticSearch
-#wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.4.3/rabbitmq-server-3.4.3-1.noarch.rpm #RabbitMQ
-wget ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/1.3/GraphicsMagick-1.3.9.tar.gz #GraphicsMagick
-#pdf2htmlEX
-#LibreOffice
-wget http://nginx.org/download/nginx-1.6.2.tar.gz #NginX
-#git clone #EtherPad-lite
-#git clone #Hilary
-#git clone #3akai-ux
-
-#Unpack all files
-tar -zxvf node-v0.10.36-linux-x64.tar.gz -C /home/oae/OAE/
-tar -zxvf node*.tar.gz -C ~/OAE
-tar -zxvf apache*.tar.gz -C ~/OAE
-tar -zxvf jre*.tar.gz -C ~/OAE
-tar -zxvf radis*.tar.gz -C ~/OAE
-tar -zxvf elasticsearch*.tar.gz -C ~/OAE
-tar -zxvf GraphicsMagick*.tar.gz -C ~/OAE
-tar -zxvf nginx*.tar.gz -C ~/OAE
-
-#Check and add to path
-echo $PATH | grep "node"
+#NODEJS-------------------------
+wget http://nodejs.org/dist/v0.10.36/node-v0.10.36-linux-x64.tar.gz #Node.js Download
+tar -zxvf node*.tar.gz -C ~/OAE #Unpack Node.JS and move to ~/OAE
+echo $PATH | grep "node" #Check to see if Node.js is in env path
 if [[ $? -eq 0 ]] ; then
 	echo 'Node is in the PATH'
 else
-	echo 'Node is not in PATH'
+	echo 'Node is not in PATH' #if its no in the path then put it in the bashrc
 	echo "export NODEJS_HOME=/home/oae/OAE/node-v0.10.36-linux-x64" >> /home/oae/.bashrc
 	echo 'export PATH=$PATH:$NODEJS_HOME/bin' >> /home/oae/.bashrc
 fi
-echo $PATH | grep "jre"
+#NODEJS-------------------------
+
+#Apache-Cassandra---------------
+#Orical Java 7
+wget sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+"http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jre-8u25-linux-x64.tar.gz" #Orical Java 7 Download
+tar -zxvf jre*.tar.gz -C ~/OAE #Unpack Java and move it to ~/OAE
+echo $PATH | grep "jre" #Check to see if Java is in the env path
 if [[ $? -eq 0 ]] ; then
 	echo 'JAVA is in PATH'
 else
-	echo 'JAVA is not in PATH'
+	echo 'JAVA is not in PATH' #if its not in the path then put it in the bashrc
 	echo "export JAVA_HOME=/home/oae/OAE/jre1.8.0_25" >> /home/oae/.bashrc
 	echo 'export PATH=$PATH:$NODEJS_HOME/bin' >> /home/oae/.bashrc
 fi
 
-#git Clone
-
-
-#Additonal
-#cassandra
+#Apache-Cassandra
+wget http://download.nextag.com/apache/cassandra/2.1.2/apache-cassandra-2.1.2-bin.tar.gz #Apache Cassandra
+tar -zxvf apache*.tar.gz -C ~/OAE #Unpack Apache-Cassandra and move to ~/OAE
 sudo mkdir -p /var/log/cassandra
 sudo chown -R oae /var/log/cassandra
 sudo mkdir -p /var/lib/cassandra
 sudo chown -R oea /var/lib/cassandra
-#redis
+#Apache-Cassandra---------------
+
+#Redis--------------------------
+wget http://download.redis.io/releases/redis-2.8.19.tar.gz #Redis Download
+sudo yum group install "Development Tools" #Dependencies
+tar -zxvf radis*.tar.gz -C ~/OAE #Unpack Redis and Move it to ~/OAE
 cd ~/OAE/redis-2.8.19
 make
 sudo make install
-#rabbitmq
+#Redis--------------------------
+
 cd ~/Downloads
-#sudo rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-#yum install rabbitmq-server-3.2.2-1.noarch.rpm
+
+#ElasticSearch------------------
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.2.tar.gz #ElasticSearch
+tar -zxvf elasticsearch*.tar.gz -C ~/OAE
+#ElasticSearch------------------
+
+#RabbitMQ-----------------------
+su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm'
+sudo yum install erlang
+sudo rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 rpm -Uvh http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.4/rabbitmq-server-3.1.4-1.noarch.rpm
-#GraphicsMagick
+#RabbitMQ-----------------------
+
+#GraphicsMagick-----------------
+sudo yum install -y gcc libpng libjpeg libpng-devel libjpeg-devel ghostscript
+sudo yum install -y libtiff libtiff-devel freetype freetype-devel
+wget ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/1.3/GraphicsMagick-1.3.9.tar.gz #GraphicsMagick
+tar -zxvf GraphicsMagick*.tar.gz -C ~/OAE
 cd ~/OAE/GraphicsMagick-1.3.9
 ./configure --enable-shared
 make
 sudo make install
+#GraphicsMagick-----------------
 
+cd ~/Downloads
 
-#run commands
-/home/oae/OAE/apache-cassandra-2.1.2/bin/cassandra
-/home/oae/OAE/redis-2.8.19/src/redis-server &
-/home/oae/OAE/elasticsearch-1.1.2/bin/elasticsearch &
-rabbitmq-server -detached
+#Preview-Processor--------------
+#pdf2htmlEX---------------------
+#pdf2htmlEX---------------------
 
+#LibreOffice--------------------
+#LibreOffice--------------------
+#Preview-Processor--------------
+
+#Nginx--------------------------
+#PCRE
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.35.tar.gz #PCRE
+tar -zxvf pcre*.tar.gz -C ~/OAE
+
+#Nginx
+wget http://nginx.org/download/nginx-1.6.2.tar.gz #NginX
+tar -zxvf nginx*.tar.gz -C ~/OAE
+cd ~/OAE/nginx-1.6.2/
+./configure --with-pcre=/home/oae/OAE/pcre-8.35/
+make
+sudo make install
+#Nginx--------------------------
+
+#Etherpad-lite------------------
+cd ~/OAE/
+sudo yum install gzip git curl python openssl-devel && yum groupinstall "Development Tools"
+git clone git://github.com/ether/etherpad-lite.git
+cd etherpad-lite
+bin/run.sh
+cd node_modules
+git clone https://github.com/oaeproject/ep_oae
+cd ep_oae
+npm install
+cp static/css/pad.css ../../src/static/custom/pad.css
+cd ../..
+sed -i "s/\"xhr-polling/\"websocket\"\, \"xhr-polling/g" settings.json
+sed -i '/defaultPadText/c \"defaultPadText\"\ \:\ \"\"' settings.json
+#EtherPad-lite------------------
+
+#Hilary/3akai-ux----------------
+cd ~/OAE
+git clone git://github.com/oaeproject/Hilary.git
+git clone git://github.com/oaeproject/3akai-ux.git
+sudo bash -c "echo '127.0.0.1  admin.oakland.edu' >> /etc/hosts"
+sudo bash -c "echo '127.0.0.1  oae.oakland.edu' >> /etc/hosts"
+mkdir /home/oae/OAE/uploads
+#Hilary/3akai-ux----------------
